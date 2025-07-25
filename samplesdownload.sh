0e215155fb2c593022e5712f07b2831478ec1ed4
Gennady Oleshko <gennady@oleshko-g.ru>
2025-07-25 20:56:30 +0300

    update samplesdownload.sh with the full curl flags

--- samplesdownload.sh
+++ samplesdownload.sh
@@ -12,5 +12,5 @@ image_urls=(
 
 for url in "${image_urls[@]}"; do
   file_name=$(basename "$url")
-  curl -sSfL -o "samples/$file_name" "$url"
+  curl --silent --show-error --fail --location --output "samples/$file_name" "$url"
 done
