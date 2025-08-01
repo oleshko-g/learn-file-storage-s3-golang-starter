d79fb1fad98302e7df472268a5d6c5cbc8a65844
Gennady Oleshko <gennady@oleshko-g.ru>
2025-08-01 20:49:24 +0300

    refactor saving asset to disk

--- assets.go
+++ assets.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +13,18 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func saveFileToDisk(src io.Reader, path, ext string) (filePath string, err error) {
+	randomData := make([]byte, 32)
+	rand.Read(randomData)
+	filePath = filepath.Join(path, base64.RawURLEncoding.EncodeToString(randomData)) + "." + ext
+
+	fileOnDisk, err := os.Create(filePath)
+	if err == nil {
+		defer fileOnDisk.Close()
+		_, err = io.Copy(fileOnDisk, src)
+	}
+
+	return filePath, err
 }
